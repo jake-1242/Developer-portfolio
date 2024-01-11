7893c0e185da9abe3a84b87dc9034c1ae6a6611a
Jake <jakebarry1242@gmail.com>
2024-01-11 16:50:00 +0000

    corrected some of the math that would allow for two decimal places. removed amount of tippers until it can be fixed.

--- index.js
+++ index.js
@@ -1,76 +1,35 @@
-const form = document.getElementById('calculator')
 
-console.log (form)
 
-form.onsubmit= function (event){
+const form = document.getElementById("calculator")
+
+ 
+
+form.onsubmit=function(event){
     event.preventDefault()
-    
 
-// inputs
-    let howMuch = document.getElementById('totalBeforeTip')
-    let tipAmount = document.getElementById('tipPercentage')
-    let noPeople = document.getElementById('numberToSplit')
+// input
+let cost = document.getElementById("totalBeforeTip")
+let whatTip = document.getElementById("tipPercentage")
+let noPeople = document.getElementById('numberToSplit')
 
-// outputs
-    let totalBill = document.getElementById('total-bill')
-    let totalPercentage = document.getElementById('total-tip')
-    let totalPerPerson = document.getElementById('total-per-person')
+let preTipTotal =parseFloat(cost.value)
+let tipPercentage =parseFloat(whatTip.value)
 
+// output
+let totalBill = document.getElementById('total-bill')
 
-    let cost = parseFloat(howMuch.value)
-    let customers =parseInt(noPeople.value)
-    let tip =parseFloat(tipAmount.value)
-    let percentage = 0
-
-    if (tipAmount.value !== ''){
-        percentage = parseFloat(tipAmount.value)
-    }
-
-    
-    let percentageAmount =calculatePercentageAmount(cost,tip)
-    totalPercentage.value =percentageAmount.toFixed(2)
-
-    let totalValue = calculateTotalValue(cost,percentageAmount)
-    totalBill.value = totalValue.toFixed(2)
-
-    let totalPerCustomer = calculatePerCustomer(totalValue,customers)
-    totalPerPerson.value=totalPerCustomer.toFixed(2)
-
-
-    return false
+let totalValue=tipCalculator(preTipTotal,tipPercentage)
+totalBill.value=totalValue
 
 }
 
-function calculatePercentageAmount(cost,tip){
-    return ((cost/100)*tip)
-}
 
-function calculateTotalValue(cost,percentageAmount){
-    return (cost + percentageAmount)
-}
-
-function calculatePerCustomer(totalValue,customers){
-    return (totalValue /customers)
-
-}
-
-// let totalBeforeTip = (document.getElementById('preTipTotal')).value
-
-// let tipPercentage =(document.getElementById("tipPercentage")).value
-
-// let tipToAdd = (preTipTotal/100)*tipPercentage
-
-// let finalTotal = (preTipTotal + tipToAdd)
-
-// const tipMessage =("Your total bill with tip is £")
-
-// outputTotal = () => {
-//     return document.getElementById("total").innerHTML=(`${tipMessage}${finalTotal}`)
-// }
-
-//  function resetFunction() {
-//     document.getElementById("total").reset();
-     
-//  }
-
-//  alert("hello world")
+function tipCalculator(total,percentage){
+    let preTipAmount=(total/100) * percentage;
+    let tipAmount=preTipAmount.toFixed(2);
+    let totalWithTip=preTipAmount;
+    totalWithTip += total;
+    let grandTotal = totalWithTip.toFixed(2)
+    return `Your tip is £${tipAmount}, which added to your bill comes to a grand total of £${grandTotal}`   
+  }
+  
